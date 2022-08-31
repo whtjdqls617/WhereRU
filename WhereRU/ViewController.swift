d5fb90d8f5aa84978c4f8b51247e2f21754eb12a
seocho <whtjdqls617@gmail.com>
2022-08-31 17:23:03 +0900

    Initial Commit

---
+++ WhereRU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhereRU
+//
+//  Created by 조성빈 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
