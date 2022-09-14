b032201ff1d807a462333158aba9b8c994df4d8f
seocho <whtjdqls617@gmail.com>
2022-09-14 18:29:28 +0900

    [Feat] 이메일 중복체크 후 회원가입 구현 [Feat] 로그인 구현

--- WhereRU/View/Controller/SignInViewController.swift
+++ WhereRU/View/Controller/SignInViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: BaseViewController {
 
@@ -19,5 +20,32 @@ class SignInViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        signInView.signInButton.addTarget(self, action: #selector(pressSignInButton), for: .touchUpInside)
+        
+        signInView.signUpButton.addTarget(self, action: #selector(pressSignUpButton), for: .touchUpInside)
+    }
+    
+    @objc func pressSignInButton() {
+        let email = signInView.eamilTextField.text ?? ""
+        let password = signInView.passwordTextField.text ?? ""
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if result != nil {
+                let mainVC = TabBarViewController()
+                mainVC.modalPresentationStyle = .fullScreen
+                self.present(mainVC, animated: true)
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Email과 Password를 다시 한번 확인해 주시길 바랍니다.", preferredStyle: UIAlertController.Style.alert)
+                let okAction = UIAlertAction(title: "확인", style: .default)
+                alert.addAction(okAction)
+                self.present(alert, animated: false, completion: nil)
+            }
+            
+        }
+    }
+    
+    @objc func pressSignUpButton() {
+        let signUpVC = SignUpViewController()
+        signUpVC.modalPresentationStyle = .fullScreen
+        present(signUpVC, animated: true)
     }
 }
