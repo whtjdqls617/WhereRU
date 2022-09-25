9985316cbe4cabf1b3caa78b1094de19b52f92ed
seocho <whtjdqls617@gmail.com>
2022-09-25 23:55:27 +0900

    [Feat] 대기실 생성 준비

---
+++ WhereRU/View/SelectedRoomView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectedRoomView.swift
+//  WhereRU
+//
+//  Created by 조성빈 on 2022/09/25.
+//
+
+import UIKit
+
+class SelectedRoomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
